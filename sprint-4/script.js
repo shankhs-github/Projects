7e10ebb2cef860d306768f45862299f93a90c7f3
shankhs-github <shankhs@gmail.com>
2020-06-17 19:45:37 +0530

    ADD : New issue form

--- sprint-4/script.js
+++ sprint-4/script.js
@@ -1,4 +1,14 @@
+// var first = [{
+//   title : '',
+//   category : 'Electronics',
+//   price : 399,
+//   description : 'The Best in it class under 499 dollars bookshelf speakers. They punch in their class above and compare with any speaker almost twice its price',
+// }]
+
+// localStorage.setItem('all_data1',JSON.stringify(allo))
+
+
 function open_tickets(){
     alert('open tickets')
-    
+
 }
